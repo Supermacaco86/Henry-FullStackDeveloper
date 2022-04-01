785d0f92f3cbdb0cd89cb7b8d52668ebb9fd3ec9
Kapelucito <95049947+Kapelu@users.noreply.github.com>
2022-03-31 23:53:25 -0300

    UpDate 31/03/2022

--- Modulo1-Foundations/00-IntroToCS/TablaVerdad.js
+++ Modulo1-Foundations/00-IntroToCS/TablaVerdad.js
@@ -1,43 +1,21 @@
 let a = 1 //' 1 = true, encendido'
 let b = 0 //' 0 = false, apagado'
 
-function tablaVerdad(1,0){
-    /*
-    A   B  = AND
-    1   0  =  0    
-    0   0  =  0
-    0   1  =  0
-    1   1  =  1
-    */
-    if (a > b){return b} 
-    if (a = b){return b}
-    if (a < b){return b}
-    if (a = b){return a}
-
-    console.log(`${a} & ${b} = ${b}`)
-
-
-
-    
-    if (a = b){return a}
-    if (a = b){return a}
-    if (a = b){return a}
-    if (a = b){return a}
-    
-    if (a = b){return a}
-    if (a = b){return a}
-    if (a = b){return a}
-    if (a = b){return a}
-    
-    if (a = b){return a}
-    if (a = b){return a}
-    if (a = b){return a}
-    if (a = b){return a}
-
-
+function tablaAnd(a,b){ 
+    return a && b  
 }
 
+console.log(`${a} y ${b} = ${tablaAnd(a,b)}`)
+console.log(`${b} y ${b} = ${tablaAnd(b,b)}`)
+console.log(`${b} y ${a} = ${tablaAnd(b,a)}`)
+console.log(`${a} y ${a} = ${tablaAnd(a,a)}`)
 
+/*
+A   B  = AND
+1   0  =  0    
+0   0  =  0
+0   1  =  0
+1   1  =  1*/
 
 
 /*
