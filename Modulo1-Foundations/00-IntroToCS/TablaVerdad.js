e669aa830305b88cebe04a3efcbac14bcda749d8
Kapelucito <95049947+Kapelu@users.noreply.github.com>
2022-03-31 23:14:14 -0300

    UpDate 31/03/2022

---
+++ Modulo1-Foundations/00-IntroToCS/TablaVerdad.js
@@ -0,0 +1,62 @@
+let a = 1 //' 1 = true, encendido'
+let b = 0 //' 0 = false, apagado'
+
+function tablaVerdad(1,0){
+    /*
+    A   B  = AND
+    1   0  =  0    
+    0   0  =  0
+    0   1  =  0
+    1   1  =  1
+    */
+    if (a > b){return b} 
+    if (a = b){return b}
+    if (a < b){return b}
+    if (a = b){return a}
+
+    console.log(`${a} & ${b} = ${b}`)
+
+
+
+    
+    if (a = b){return a}
+    if (a = b){return a}
+    if (a = b){return a}
+    if (a = b){return a}
+    
+    if (a = b){return a}
+    if (a = b){return a}
+    if (a = b){return a}
+    if (a = b){return a}
+    
+    if (a = b){return a}
+    if (a = b){return a}
+    if (a = b){return a}
+    if (a = b){return a}
+
+
+}
+
+
+
+
+/*
+
+// OR
+
+A   B  =  OR
+1   0  =  1
+0   1  =  1
+1   1  =  1
+0   0  =  0
+
+
+// XOR - exclusive OR ( Es uno o el otro, no pueden ser ambos)
+
+A   B  =  OR
+1   1  =  0
+0   1  =  1
+1   0  =  1
+0   0  =  0
+
+*/
