7c65813f3e066edb1ddb8b38871bb764a8596d6a
Kapelucito <95049947+Kapelu@users.noreply.github.com>
2022-04-01 10:52:55 -0300

    UpDate 01/04/2022

--- Modulo1-Foundations/00-IntroToCS/TablaVerdad.js
+++ Modulo1-Foundations/00-IntroToCS/TablaVerdad.js
@@ -1,10 +1,47 @@
+
 //$     ****   TABLAS DE LA VERDAD   ****
+/*
+? 0 = False
+? 1 = True
+$                      AND
+?                  A   B  = AND
+                   1   0  =  0    
+                   0   0  =  0
+                   0   1  =  0
+                   1   1  =  1
+
+$                      OR
+?                  A   B  =  OR
+                   1   0  =  1
+                   0   1  =  1
+                   1   1  =  1
+                   0   0  =  0
+
+$                      XOR - exclusive OR ( una salida verdadera resulta si una, y solo una de las entradas a la puerta es verdadera. Si ambas entradas son falsas o ambas son verdaderas, resulta en una salida falsa)
+?                  A   B  =  OR
+                   1   1  =  0
+                   0   1  =  1         Martina ó Belen-----> Martina y Belen (NO = false)--->[Este es exclusivo]
+                   1   0  =  1         Martina y/o Belen---> Martina y Belen (SI = true)
+                   0   0  =  0
+
+$                    NOT (es la negacion de lo que tengo)
+?                  A   NOT
+                   1    0
+                   0    1
+*/
+
+/**
+ * Tablas de la Vardad 
+ * @param {Number} a = Vale Verdadero
+ * @param {Number} b = Vale falso
+ * @returns Condicion de a con respecto a b
+ */
 
 function tablaAnd(a,b){return a && b}
 function tablaOr(a,b){ return a || b}
 function tablaNot(a,b){ return a != b}
 function tablaXor(a,b){return tablaOr(tablaAnd(a, !b), tablaAnd(!a, b))}
-let a = 1 , b = 0 
+let a = 1, b = 0 
 
 //* Tabla AND
 console.log(`${a} y ${a} = ${tablaAnd(a,a)}`)
