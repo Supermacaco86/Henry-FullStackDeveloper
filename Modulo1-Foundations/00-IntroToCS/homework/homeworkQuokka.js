8aba710706877acf627af210f2a26c79ce53d0aa
Kapelucito <95049947+Kapelu@users.noreply.github.com>
2022-04-02 23:58:45 -0300

    UpDate 01/04/2022

--- Modulo1-Foundations/00-IntroToCS/homework/homeworkQuokka.js
+++ Modulo1-Foundations/00-IntroToCS/homework/homeworkQuokka.js
@@ -1,11 +1,12 @@
-/*           PASAR DE BINARIO A DECIMAL 
+/*
+$           PASAR DE BINARIO A DECIMAL 
 (Se eleva a potencia 2 cada número binario y se suma el resultado de cada numero al final.)
-                   (10011)
-              1 x 2^0 = 1 x  1 =  1 
-              1 x 2^1 = 1 x  2 =  2
-              0 x 2^2 = 0 x  4 =  0    = 19
-              0 x 2^3 = 0 x  8 =  0
-              1 x 2^4 = 1 x 16 = 16
+?                   (10011)
+?              1 x 2^0 = 1 x  1 =  1 
+?              1 x 2^1 = 1 x  2 =  2
+?              0 x 2^2 = 0 x  4 =  0    = 19
+?              0 x 2^3 = 0 x  8 =  0
+?              1 x 2^4 = 1 x 16 = 16
 */
 function BinarioADecimal(num) {
     let sum = 0
@@ -14,13 +15,14 @@ function BinarioADecimal(num) {
 }
 console.log(BinarioADecimal("10011"))
 
-/*          PASAR DE DECIMAL A BINARIO 
-(Se divide el numero decimal en 2 hasta que el residuo sea menor a 2, luego se cuentan los residuos de derecha a izquierda tomando el último numero como residual)
-                   19
-             19 / 2 = 9 resto 1 ----> 1 
-              9 / 2 = 4 resto 1 ----> 1 
-              4 / 2 = 2 resto 0 ----> 0  
-              2 / 2 = 1 ------------> 1 
+/*
+$         PASAR DE DECIMAL A BINARIO 
+(Se divide el numero decimal en 2 hasta que el residuo sea menor a 2, luego se cuentan los residuos de derecha a izquierda tomando el último ??numero como residual)
+?                   19
+?             19 / 2 = 9 resto 1 ----> 1 
+?              9 / 2 = 4 resto 1 ----> 1 
+?              4 / 2 = 2 resto 0 ----> 0  
+?              2 / 2 = 1 ------------> 1 
 
 */
 function DecimalABinario(num){
